1a3c321b148f9bc6e5d8ae57946de4f9eb15c56c
Maurice D. Hanisch <maurice.hanisch@ibm.com>
2023-10-15 14:15:52 +0200

    method 2 Injection

--- task1_handout/hypno_script.py
+++ task1_handout/hypno_script.py
@@ -1,7 +1,23 @@
 import sys
 from hypno import inject_py
 
+print('Hello from hypno_script.py')
 if __name__ == '__main__':
+    print('Running as main')
+
+    #script_file_path = 'code.py'  # Replace with the actual path to your script file
+    #with open(script_file_path, 'r') as file:
+        #python_code = file.read()
+
+    #code_to_inject = "import os; print('Hello again from', os.getpid())"  # your Python code to inject
+    
+
+    if len(sys.argv) != 3:
+        print("Usage: hypno_script.py <PID> <code_to_inject>")
+        sys.exit(1)
+
     pid = int(sys.argv[1])
-    code_to_inject = '...'  # your Python code to inject
+    code_to_inject = sys.argv[2]  # Get the code to inject as the second command-line argument
     inject_py(pid, code_to_inject)
+
+    
--- task1_handout/runner.sh
+++ task1_handout/runner.sh
@@ -1,4 +1,5 @@
-docker build --tag task1 .
-docker run --rm -v "$( cd "$( dirname "$0" )" && pwd )":/results task1
+docker build -t task1 .
+#docker run --rm -v "$( cd "$( dirname "$0" )" && pwd )":/results task1
+docker run --rm -v "$( cd "$( dirname "$0" )" && pwd )":/results --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --security-opt apparmor=unconfined task1
 #docker run --rm -it -v "$( cd "$( dirname "$0" )" && pwd )":/results task1 /bin/bash
 
