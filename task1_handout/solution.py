63d02d8f08aae58a11e212438771f1261055f023
Maurice D. Hanisch <maurice.hanisch@ibm.com>
2023-10-19 14:43:31 +0200

    original handout back on main

--- task1_handout/solution.py
+++ task1_handout/solution.py
@@ -3,10 +3,9 @@ import typing
 from sklearn.gaussian_process.kernels import *
 import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.kernel_approximation import Nystroem
 import matplotlib.pyplot as plt
 from matplotlib import cm
-# os.chdir('C:\\Users\\MOUms\\VS Projects\\PAI_v2\\task1_handout')
+
 
 # Set `EXTENDED_EVALUATION` to `True` in order to visualize your predictions.
 EXTENDED_EVALUATION = False
@@ -30,7 +29,6 @@ class Model(object):
         We already provide a random number generator for reproducibility.
         """
         self.rng = np.random.default_rng(seed=0)
-        self.gp = None
 
         # TODO: Add custom initialization for your model here if necessary
 
@@ -44,33 +42,26 @@ class Model(object):
             containing your predictions, the GP posterior mean, and the GP posterior stddev (in that order)
         """
 
-        gp_mean, gp_std = self.gp.predict(test_x_2D, return_std=True)
-        adjustment = np.where(test_x_AREA, gp_std, 0)
-        predictions = gp_mean + adjustment
+        # TODO: Use your GP to estimate the posterior mean and stddev for each city_area here
+        gp_mean = np.zeros(test_x_2D.shape[0], dtype=float)
+        gp_std = np.zeros(test_x_2D.shape[0], dtype=float)
+
+        # TODO: Use the GP posterior to form your predictions here
+        predictions = gp_mean
 
         return predictions, gp_mean, gp_std
 
-    def fitting_model(self, train_y: np.ndarray, train_x_2D: np.ndarray):
+    def fitting_model(self, train_y: np.ndarray,train_x_2D: np.ndarray):
         """
         Fit your model on the given training data.
         :param train_x_2D: Training features as a 2d NumPy float array of shape (NUM_SAMPLES, 2)
         :param train_y: Training pollution concentrations as a 1d NumPy float array of shape (NUM_SAMPLES,)
         """
-        # Use the generator to produce an integer seed
-        seed = self.rng.integers(low=0, high=4294967295)
 
-        # nystroem = Nystroem(kernel='rbf', gamma=1.0, n_components=int(
-        # 0.01*train_y.shape[0]), random_state=0)
-        # X_nystroem = nystroem.fit_transform(train_x_2D)
-        kernel = 1.0 * RBF(length_scale=1.0)
-        self.gp = GaussianProcessRegressor(
-            kernel=kernel, n_restarts_optimizer=20, random_state=seed)
-        # self.gp.fit(X_nystroem, train_y)
-        self.gp.fit(train_x_2D, train_y)
+        # TODO: Fit your model here
+        pass
 
 # You don't have to change this function
-
-
 def cost_function(ground_truth: np.ndarray, predictions: np.ndarray, AREA_idxs: np.ndarray) -> float:
     """
     Calculates the cost of a set of predictions.
@@ -87,8 +78,7 @@ def cost_function(ground_truth: np.ndarray, predictions: np.ndarray, AREA_idxs: 
     weights = np.ones_like(cost) * COST_W_NORMAL
 
     # Case i): underprediction
-    mask = (predictions < ground_truth) & [
-        bool(AREA_idx) for AREA_idx in AREA_idxs]
+    mask = (predictions < ground_truth) & [bool(AREA_idx) for AREA_idx in AREA_idxs]
     weights[mask] = COST_W_UNDERPREDICT
 
     # Weigh the cost and return the average
@@ -105,9 +95,7 @@ def is_in_circle(coor, circle_coor):
     """
     return (coor[0] - circle_coor[0])**2 + (coor[1] - circle_coor[1])**2 < circle_coor[2]**2
 
-# You don't have to change this function
-
-
+# You don't have to change this function 
 def determine_city_area_idx(visualization_xs_2D):
     """
     Determines the city_area index for each coordinate in the visualization grid.
@@ -116,29 +104,27 @@ def determine_city_area_idx(visualization_xs_2D):
     """
     # Circles coordinates
     circles = np.array([[0.5488135, 0.71518937, 0.17167342],
-                        [0.79915856, 0.46147936, 0.1567626],
-                        [0.26455561, 0.77423369, 0.10298338],
-                        [0.6976312,  0.06022547, 0.04015634],
-                        [0.31542835, 0.36371077, 0.17985623],
-                        [0.15896958, 0.11037514, 0.07244247],
-                        [0.82099323, 0.09710128, 0.08136552],
-                        [0.41426299, 0.0641475,  0.04442035],
-                        [0.09394051, 0.5759465,  0.08729856],
-                        [0.84640867, 0.69947928, 0.04568374],
-                        [0.23789282, 0.934214,   0.04039037],
-                        [0.82076712, 0.90884372, 0.07434012],
-                        [0.09961493, 0.94530153, 0.04755969],
-                        [0.88172021, 0.2724369,  0.04483477],
-                        [0.9425836,  0.6339977,  0.04979664]])
-
+                    [0.79915856, 0.46147936, 0.1567626 ],
+                    [0.26455561, 0.77423369, 0.10298338],
+                    [0.6976312,  0.06022547, 0.04015634],
+                    [0.31542835, 0.36371077, 0.17985623],
+                    [0.15896958, 0.11037514, 0.07244247],
+                    [0.82099323, 0.09710128, 0.08136552],
+                    [0.41426299, 0.0641475,  0.04442035],
+                    [0.09394051, 0.5759465,  0.08729856],
+                    [0.84640867, 0.69947928, 0.04568374],
+                    [0.23789282, 0.934214,   0.04039037],
+                    [0.82076712, 0.90884372, 0.07434012],
+                    [0.09961493, 0.94530153, 0.04755969],
+                    [0.88172021, 0.2724369,  0.04483477],
+                    [0.9425836,  0.6339977,  0.04979664]])
+    
     visualization_xs_AREA = np.zeros((visualization_xs_2D.shape[0],))
 
-    for i, coor in enumerate(visualization_xs_2D):
-        visualization_xs_AREA[i] = any(
-            [is_in_circle(coor, circ) for circ in circles])
+    for i,coor in enumerate(visualization_xs_2D):
+        visualization_xs_AREA[i] = any([is_in_circle(coor, circ) for circ in circles])
 
     return visualization_xs_AREA
-
 
 # You don't have to change this function
 def perform_extended_evaluation(model: Model, output_dir: str = '/results'):
@@ -151,22 +137,16 @@ def perform_extended_evaluation(model: Model, output_dir: str = '/results'):
 
     # Visualize on a uniform grid over the entire coordinate system
     grid_lat, grid_lon = np.meshgrid(
-        np.linspace(0, EVALUATION_GRID_POINTS - 1,
-                    num=EVALUATION_GRID_POINTS) / EVALUATION_GRID_POINTS,
-        np.linspace(0, EVALUATION_GRID_POINTS - 1,
-                    num=EVALUATION_GRID_POINTS) / EVALUATION_GRID_POINTS,
+        np.linspace(0, EVALUATION_GRID_POINTS - 1, num=EVALUATION_GRID_POINTS) / EVALUATION_GRID_POINTS,
+        np.linspace(0, EVALUATION_GRID_POINTS - 1, num=EVALUATION_GRID_POINTS) / EVALUATION_GRID_POINTS,
     )
-    visualization_xs_2D = np.stack(
-        (grid_lon.flatten(), grid_lat.flatten()), axis=1)
+    visualization_xs_2D = np.stack((grid_lon.flatten(), grid_lat.flatten()), axis=1)
     visualization_xs_AREA = determine_city_area_idx(visualization_xs_2D)
-
+    
     # Obtain predictions, means, and stddevs over the entire map
-    predictions, gp_mean, gp_stddev = model.make_predictions(
-        visualization_xs_2D, visualization_xs_AREA)
-    predictions = np.reshape(
-        predictions, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
-    gp_mean = np.reshape(
-        gp_mean, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
+    predictions, gp_mean, gp_stddev = model.make_predictions(visualization_xs_2D, visualization_xs_AREA)
+    predictions = np.reshape(predictions, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
+    gp_mean = np.reshape(gp_mean, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
 
     vmin, vmax = 0.0, 65.0
 
@@ -174,7 +154,7 @@ def perform_extended_evaluation(model: Model, output_dir: str = '/results'):
     fig, ax = plt.subplots()
     ax.set_title('Extended visualization of task 1')
     im = ax.imshow(predictions, vmin=vmin, vmax=vmax)
-    cbar = fig.colorbar(im, ax=ax)
+    cbar = fig.colorbar(im, ax = ax)
 
     # Save figure to pdf
     figure_path = os.path.join(output_dir, 'extended_evaluation.pdf')
@@ -192,10 +172,12 @@ def extract_city_area_information(train_x: np.ndarray, test_x: np.ndarray) -> ty
     :return: Tuple of (training features' 2D coordinates, training features' city_area information,
         test features' 2D coordinates, test features' city_area information)
     """
-    train_x_2D = train_x[:, :2]
-    train_x_AREA = train_x[:, 2].astype(bool)
-    test_x_2D = test_x[:, :2]
-    test_x_AREA = test_x[:, 2].astype(bool)
+    train_x_2D = np.zeros((train_x.shape[0], 2), dtype=float)
+    train_x_AREA = np.zeros((train_x.shape[0],), dtype=bool)
+    test_x_2D = np.zeros((test_x.shape[0], 2), dtype=float)
+    test_x_AREA = np.zeros((test_x.shape[0],), dtype=bool)
+
+    #TODO: Extract the city_area information from the training and test features
 
     assert train_x_2D.shape[0] == train_x_AREA.shape[0] and test_x_2D.shape[0] == test_x_AREA.shape[0]
     assert train_x_2D.shape[1] == 2 and test_x_2D.shape[1] == 2
@@ -204,31 +186,18 @@ def extract_city_area_information(train_x: np.ndarray, test_x: np.ndarray) -> ty
     return train_x_2D, train_x_AREA, test_x_2D, test_x_AREA
 
 # you don't have to change this function
-
-
 def main():
     # Load the training dateset and test features
-    print('Loading data')
     train_x = np.loadtxt('train_x.csv', delimiter=',', skiprows=1)
     train_y = np.loadtxt('train_y.csv', delimiter=',', skiprows=1)
     test_x = np.loadtxt('test_x.csv', delimiter=',', skiprows=1)
 
-    # Take a random subset of the training data
-    print('Taking a random subset of the training data')
-    percentage = 0.2
-    random_indices = np.random.choice(train_y.shape[0], int(
-        percentage/100 * train_y.shape[0]), replace=False)
-    train_x = train_x[random_indices]
-    train_y = train_y[random_indices]
-
     # Extract the city_area information
-    train_x_2D, train_x_AREA, test_x_2D, test_x_AREA = extract_city_area_information(
-        train_x, test_x)
-
+    train_x_2D, train_x_AREA, test_x_2D, test_x_AREA = extract_city_area_information(train_x, test_x)
     # Fit the model
     print('Fitting model')
     model = Model()
-    model.fitting_model(train_y, train_x_2D)
+    model.fitting_model(train_y,train_x_2D)
 
     # Predict on the test features
     print('Predicting on test features')
